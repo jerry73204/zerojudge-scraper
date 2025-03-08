f146f5ef73c34986b4f6a95e53c509a4f238ced0
aeon <jerry73204@gmail.com>
2025-03-08 14:08:17 +0800

    Save the submission ID for c292.

--- c292/solution/main.c
+++ c292/solution/main.c
@@ -1,3 +1,8 @@
+/*
+  Submission ID: 15613535
+  AC (2ms, 96KB)
+ */
+
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,62 +11,62 @@ static int ROW_STEPS[4] = {0, -1, 0, 1};
 static int COL_STEPS[4] = {-1, 0, 1, 0};
 
 int main() {
-  /* Read the matrix size */
-  int n;
-  scanf("%d", &n);
+    /* Read the matrix size */
+    int n;
+    scanf("%d", &n);
 
-  /* Read the initial direction */
-  int direction;
-  scanf("%d", &direction);
+    /* Read the initial direction */
+    int direction;
+    scanf("%d", &direction);
 
-  /* Read the matrix */
-  int *matrix = (int *)malloc(n * n * sizeof(int));
-  assert(matrix != NULL);
+    /* Read the matrix */
+    int *matrix = (int *)malloc(n * n * sizeof(int));
+    assert(matrix != NULL);
 
-  for (int *ptr = matrix; ptr != matrix + n * n; ptr += 1) {
-    scanf("%d", ptr);
-  }
+    for (int *ptr = matrix; ptr != matrix + n * n; ptr += 1) {
+        scanf("%d", ptr);
+    }
 
-  /* Initialize the position */
-  int row = n / 2;
-  int col = n / 2;
+    /* Initialize the position */
+    int row = n / 2;
+    int col = n / 2;
 
-  /* Traverse the matrix elements in the spiral order.
+    /* Traverse the matrix elements in the spiral order.
 
-     The traversal consists of two loops. The outer loop counts the
-     "steps" from 1 to n-1. The inner loop repeats 2 times. In each
-     inner loop, the cursor moves "steps" times, in which each move
-     prints the element.
-   */
-  for (int steps = 1; steps < n; steps += 1) {
-    for (int i = 0; i < 2; i += 1) {
-      int rs = ROW_STEPS[direction];
-      int cs = COL_STEPS[direction];
+       The traversal consists of two loops. The outer loop counts the
+       "steps" from 1 to n-1. The inner loop repeats 2 times. In each
+       inner loop, the cursor moves "steps" times, in which each move
+       prints the element.
+     */
+    for (int steps = 1; steps < n; steps += 1) {
+        for (int i = 0; i < 2; i += 1) {
+            int rs = ROW_STEPS[direction];
+            int cs = COL_STEPS[direction];
 
-      for (int j = 0; j < steps; j += 1) {
+            for (int j = 0; j < steps; j += 1) {
+                printf("%d", matrix[row * n + col]);
+                row += rs;
+                col += cs;
+            }
+
+            direction = (direction + 1) % 4;
+        }
+    }
+
+    /* Print remaining elements */
+    int rs = ROW_STEPS[direction];
+    int cs = COL_STEPS[direction];
+    for (int j = 0; j < n; j += 1) {
         printf("%d", matrix[row * n + col]);
         row += rs;
         col += cs;
-      }
-
-      direction = (direction + 1) % 4;
     }
-  }
 
-  /* Print remaining elements */
-  int rs = ROW_STEPS[direction];
-  int cs = COL_STEPS[direction];
-  for (int j = 0; j < n; j += 1) {
-    printf("%d", matrix[row * n + col]);
-    row += rs;
-    col += cs;
-  }
+    /* Print the trailing line break. */
+    printf("\n");
 
-  /* Print the trailing line break. */
-  printf("\n");
+    /* Finalize */
+    free(matrix);
 
-  /* Finalize */
-  free(matrix);
-
-  return 0;
+    return 0;
 }
