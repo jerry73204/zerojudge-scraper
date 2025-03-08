c43efbe7320f9808ea18b659584418b66e0ce50a
aeon <jerry73204@gmail.com>
2025-03-08 14:10:34 +0800

    Fix c294 solution and save the submission ID.

--- c294/solution/main.c
+++ c294/solution/main.c
@@ -1,61 +1,69 @@
+/*
+  Submission ID: 15613545
+  AC (2ms, 92KB)
+ */
+
 #include <assert.h>
 #include <stdio.h>
 
 int main() {
-  /* Read the side lengths of a triangle */
-  int ia, ib, ic;
-  scanf("%d %d %d", &ia, &ib, &ic);
+    /* Read the side lengths of a triangle */
+    int ia, ib, ic;
+    scanf("%d %d %d", &ia, &ib, &ic);
 
-  /* Sort the side lengths */
-  int sa, sb, sc;
-  if (ia > ib) {
-    if (ia < ic) {
-      sa = ib;
-      sb = ia;
-      sc = ic;
-    } else if (ib > ic) {
-      sa = ic;
-      sb = ib;
-      sc = ia;
+    /* Sort the side lengths */
+    int sa, sb, sc;
+    if (ia > ib) {
+        if (ia < ic) {
+            sa = ib;
+            sb = ia;
+            sc = ic;
+        } else if (ib > ic) {
+            sa = ic;
+            sb = ib;
+            sc = ia;
+        } else {
+            sa = ib;
+            sb = ic;
+            sc = ia;
+        }
     } else {
-      sa = ib;
-      sb = ic;
-      sc = ia;
+        if (ib < ic) {
+            sa = ia;
+            sb = ib;
+            sc = ic;
+        } else if (ia > ic) {
+            sa = ic;
+            sb = ia;
+            sc = ib;
+        } else {
+            sa = ia;
+            sb = ic;
+            sc = ib;
+        }
     }
-  } else {
-    if (ib < ic) {
-      sa = ia;
-      sb = ib;
-      sc = ic;
-    } else if (ia > ic) {
-      sa = ic;
-      sb = ia;
-      sc = ib;
+
+    assert(sa <= sb && sb <= sc);
+
+    /* Print the side lengths in the ascending order */
+    printf("%d %d %d\n", sa, sb, sc);
+
+    /* Determine the type of the triangle. */
+    if (sa + sb <= sc) {
+        printf("No\n");
     } else {
-      sa = ia;
-      sb = ic;
-      sc = ib;
+
+        int ab_sqsum = sa * sa + sb * sb;
+        int c_sq = sc * sc;
+
+        if (ab_sqsum < c_sq) {
+            printf("Obtuse\n");
+        } else if (ab_sqsum > c_sq) {
+            printf("Acute\n");
+        } else {
+            printf("Right\n");
+        }
     }
-  }
 
-  assert(sa <= sb && sb <= sc);
-
-  /* Determine the type of the triangle. */
-  if (sa + sb <= sc) {
-    printf("No\n");
-  } else {
-
-    int ab_sqsum = sa * sa + sb * sb;
-    int c_sq = sc * sc;
-
-    if (ab_sqsum < c_sq) {
-      printf("Obtuse\n");
-    } else if (ab_sqsum > c_sq) {
-      printf("Acute\n");
-    } else {
-      printf("Right\n");
-    }
-  }
-
-  return 0;
+    return 0;
 }
