bfbb4d79939bb275189363f28582a7ddd3e9b083
aeon <jerry73204@gmail.com>
2025-03-08 14:06:11 +0800

    Save the submission ID for c290.

--- c290/solution/main.c
+++ c290/solution/main.c
@@ -1,43 +1,48 @@
+/*
+  Submission ID: 15613523
+  AC (6ms, 92KB)
+ */
+
 #include <stdio.h>
 
 int abs(int n) {
-  if (n >= 0) {
-    return n;
-  } else {
-    return -n;
-  }
+    if (n >= 0) {
+        return n;
+    } else {
+        return -n;
+    }
 }
 
 int main() {
-  /* a_sum and b_sum are the sums of digits in odd end even
-     places. */
-  int a_sum = 0;
-  int b_sum = 0;
+    /* a_sum and b_sum are the sums of digits in odd end even
+       places. */
+    int a_sum = 0;
+    int b_sum = 0;
 
-  /* The number of the digits of the number. */
-  int length = 0;
+    /* The number of the digits of the number. */
+    int length = 0;
 
-  /* Read digits one-by-one until a line break is encountered. */
-  while (1) {
-    char ch;
-    scanf("%c", &ch);
+    /* Read digits one-by-one until a line break is encountered. */
+    while (1) {
+        char ch;
+        scanf("%c", &ch);
 
-    if (ch == '\n') {
-      break;
-    } else {
-      length += 1;
+        if (ch == '\n') {
+            break;
+        } else {
+            length += 1;
 
-      if (length & 1) {
-        a_sum += (int)(ch - '0');
-      } else {
-        b_sum += (int)(ch - '0');
-      }
+            if (length & 1) {
+                a_sum += (int)(ch - '0');
+            } else {
+                b_sum += (int)(ch - '0');
+            }
+        }
     }
-  }
 
-  /* Print the absolute difference of odd and even digits. */
-  int ans = abs(a_sum - b_sum);
-  printf("%d\n", ans);
+    /* Print the absolute difference of odd and even digits. */
+    int ans = abs(a_sum - b_sum);
+    printf("%d\n", ans);
 
-  return 0;
+    return 0;
 }
